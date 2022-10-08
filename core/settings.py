e2f771004eb3768f9c2a0b3365907c191f6658ef
abbasKareem <kareemabbas106@gmail.com>
2022-10-08 20:27:01 +0300

    remove static from core

--- core/settings.py
+++ core/settings.py
@@ -288,9 +288,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'core/static')
+# ]
 
 
 # Default primary key field type
